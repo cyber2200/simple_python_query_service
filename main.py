2fa7357bc609e57b607932f91d7230884d991ec2
dev <dev@Argamans-MacBook-Air.local>
2022-11-15 22:53:11 +0200

    Bugs...

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import JSONResponse
 from models.Qbody import Qbody
 from models.Db import Db
 
 app = FastAPI()
 
+# --------------------------------------------------------
+qbody_example = {
+    "res": "OK",
+    "q_res": [
+        {
+            "id": "1",
+            "msg": "123",
+            "ts": "2022-11-15 19:45:12"
+        }
+    ]
+}
+
 @app.post("/q")
-async def q(qbody: Qbody):
+async def q(qbody: Qbody = Body(example = qbody_example)):
     db = Db()
     res = db.q(qbody.q, qbody.db)
     return JSONResponse(content=res)
+# --------------------------------------------------------
--- models/Db.py
+++ models/Db.py
@@ -1,28 +1,43 @@
 import mysql.connector
+import configparser
+import json
 
 class Db:
+    # Get config from json file
+    def getConfigData(self):
+        f = open('config.json') 
+        data = json.load(f)
+        return data
+    
+    # get MySQL connection
     def getConnection(self, db):
+        config = self.getConfigData()
         if db != '':
             connection = mysql.connector.connect(
-                host="localhost",
-                user="dct_mysql01",
-                password="123qwe",
+                host=config['host'],
+                user=config['user'],
+                password=config['password'],
                 database=db
             )
         else: # If there is no db selected
             connection = mysql.connector.connect(
-                host="localhost",
-                user="dct_mysql01",
-                password="123qwe"
+                host=config['host'],
+                user=config['user'],
+                password=config['password']
             )
         return connection
+    
+    # Query and format the data
     def q(self, q, db):
-        # Split query
+        # Split query - if there is more than one
         queries = q.split(';');
         queries = list(filter(None, queries))
 
-        q_res = []
+        res = []
+        # Passing on all the queries
+        qc = 0
         for q in queries:
+            q_res = []
             try:
                 connection = self.getConnection(db)
                 
@@ -39,28 +54,33 @@ class Db:
                     connection.commit()
                 connection.close()
 
-                if q.lower().startswith('select * from'):
+                # If the query is select * ... add the columns to the result
+                if q.lower().strip().startswith('select * from'):
                     connection = self.getConnection(db)
-                    table_name = q.split(' ')[3]
+                    table_name = q.strip().split(' ')[3]
                     cursor = connection.cursor()
                     cursor.execute('DESC ' + table_name)
                     table_desc = cursor.fetchall()
                     column_names = []
-                    for column_name in table_desc:
-                        column_names.append(column_name[0])
-                    
+                    for column in table_desc:
+                        column_names.append(column[0])
                     rows = []
-                    for row in q_res:
+                    print('---------------------')
+                    for row2 in q_res:
                         t_row = {}
                         i = 0
                         for column_name in column_names:
-                            t_row[column_name] = row[i]
+                            print(row2)
+                            print(i)
+                            t_row[column_name] = row2[i]
                             i = i + 1
                         rows.append(t_row)
 
                     q_res = rows
-                    
+                # Append to res and increase counter
+                res.append({'q' + str(qc): q_res})
+                qc = qc + 1
             except mysql.connector.Error as err:
                 return {'res': 'NOK', 'err': str(err)}
         
-        return {'res': 'OK', 'q_res': q_res}
+        return {'res': 'OK', 'res': res}
